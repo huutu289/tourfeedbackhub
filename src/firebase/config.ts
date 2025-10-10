6dbef0d37d8e9006a30ecc426f8402b5cd2d9642
Tu Cao Hữu <huutu289@gmail.com>
2025-10-10 05:32:40 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7375425544-d5fb9",
-  "appId": "1:970968832839:web:d35c3b5fd8f67edb430a62",
-  "apiKey": "AIzaSyAQq-YIbRCT3S2yKcBM-Dm07Ey0YpuLEVQ",
-  "authDomain": "studio-7375425544-d5fb9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "970968832839"
+  apiKey: "AIzaSyCaQBiCBPvnKq8_GDHEAiwHd_1dJ5QTgJg",
+  authDomain: "tourfeedbackhub-474704.firebaseapp.com",
+  projectId: "tourfeedbackhub-474704",
+  storageBucket: "tourfeedbackhub-474704.firebasestorage.app",
+  messagingSenderId: "965278520533",
+  appId: "1:965278520533:web:36743a0d587a81c4c63e5c",
+  measurementId: "G-V8XTJLDVCV"
 };
