deda821706bc039f17c14dba9c2b060df1219b7f
Tu Cao Hữu <huutu289@gmail.com>
2025-10-10 05:32:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7375425544-d5fb9",
+  "appId": "1:970968832839:web:d35c3b5fd8f67edb430a62",
+  "apiKey": "AIzaSyAQq-YIbRCT3S2yKcBM-Dm07Ey0YpuLEVQ",
+  "authDomain": "studio-7375425544-d5fb9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "970968832839"
+};
